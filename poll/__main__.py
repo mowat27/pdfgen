f277c2e0be00b7ea05cb3c299831b269f90e41c5
Adrian Mowat <adrian.mowat@gmail.com>
2020-12-31 22:33:01 +0000

    Added message class

--- poll/__main__.py
+++ poll/__main__.py
@@ -32,29 +32,20 @@ class Queue:
         "WaitTimeSeconds": 10
     }
 
-    def __init__(self, queue_url, *, handlers=None, sqs_options=None):
+    def __init__(self, queue_url, *, sqs_options=None):
         self.queue_url = queue_url
-        self.handlers = handlers or []
         self.sqs_options = dict(self.default_sqs_options)  # makes a copy
         self.sqs_options.update(sqs_options or {})
 
-    def add_handler(self, f):
-        self.handlers.append(f)
-
     def __next__(self):
-        return sqs.receive_message(QueueUrl=self.queue_url, **self.sqs_options)
+        data = sqs.receive_message(
+            QueueUrl=self.queue_url,
+            **self.sqs_options)
+        return Response(self, data)
 
-    def start_poller(self, *, poll_interval=1, sqs_options=None):
-        while True:
-            for message in Response(self, next(self)):
-                for handler in self.handlers:
-                    handler(message)
-
-                sqs.delete_message(
-                    QueueUrl=self.queue_url,
-                    ReceiptHandle=message['ReceiptHandle'])
-
-            time.sleep(poll_interval)
+    def delete_message(self, receipt_handle):
+        sqs.delete_message(QueueUrl=self.queue_url,
+                           ReceiptHandle=receipt_handle)
 
 
 class Response:
@@ -62,49 +53,77 @@ class Response:
         self.queue = queue
         self.response = response
 
-    def __iter__(self):
+    def messages(self):
         if 'Messages' in self.response:
             for message in self.response['Messages']:
-                yield message
+                yield Message(self.queue, message)
+
+
+class Message:
+    def __init__(self, queue, message):
+        self.queue = queue
+        self.message = message
+
+    @ property
+    def body(self):
+        return json.loads(self.message['Body'])
+
+    @ property
+    def records(self):
+        for record in self.body['Records']:
+            yield record
+
+    @ property
+    def receipt_handle(self):
+        return self.message['ReceiptHandle']
 
 
 class S3Object:
     def __init__(self, record):
         self.record = record
 
-    @property
+    @ property
     def bucket(self):
         return self.record["s3"]["bucket"]["name"]
 
-    @property
+    @ property
     def key(self):
         return self.record["s3"]["object"]["key"]
 
-    @property
+    @ property
     def filename(self):
         return os.path.basename(self.key)
 
-    @property
+    @ property
     def prefix(self):
         return os.path.dirname(self.key)
 
-    @property
+    @ property
     def s3_url(self):
         return f's3://{self.bucket}{self.prefix}/{self.filename}'
 
-    @property
+    @ property
     def metadata(self):
         return s3.head_object(Bucket=self.bucket, Key=self.key)
 
 
 def stdout_logger(message):
-    body = json.loads(message['Body'])
-
-    for obj in [S3Object(record) for record in body['Records']]:
+    for obj in [S3Object(record) for record in message.records]:
         log = f"object={obj.s3_url} callback={obj.metadata['Metadata']['callback-url']}"
         print(log)
 
 
+def start_poller(queue, *, poll_interval=1, handlers=[]):
+    while True:
+        for message in next(queue).messages():
+            for handler in handlers:
+                handler(message)
+            queue.delete_message(message.receipt_handle)
+        time.sleep(poll_interval)
+
+
 queue = Queue(SQS_OUTPUT_QUEUE)
-queue.add_handler(stdout_logger)
-queue.start_poller(poll_interval=POLL_INTERVAL_SECONDS)
+print('Starting...')
+print(f'Polling Interval: {POLL_INTERVAL_SECONDS} second(s)')
+start_poller(queue, poll_interval=POLL_INTERVAL_SECONDS,
+             handlers=[stdout_logger])
