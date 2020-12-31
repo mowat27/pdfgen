8b80803eed8dc90514e623e4f9f35aa7b7e850cc
Adrian Mowat <adrian.mowat@gmail.com>
2020-12-31 21:14:53 +0000

    Added queue with listeners to poller

--- poll/__main__.py
+++ poll/__main__.py
@@ -2,40 +2,72 @@ import boto3
 import json
 import time
 
+from signal import signal, SIGINT
+
 from . import SQS_OUTPUT_QUEUE, POLL_INTERVAL_SECONDS
 
 sqs = boto3.client('sqs')
 s3 = boto3.client('s3')
 
-while True:
-    response = sqs.receive_message(
-        QueueUrl=SQS_OUTPUT_QUEUE,
-        AttributeNames=[
+
+def exit_with_style(signal_received, frame):
+    print('\nShutting down...')
+    exit(0)
+
+
+signal(SIGINT, exit_with_style)
+
+
+class Queue:
+    default_sqs_options = {
+        "AttributeNames": [
             'SentTimestamp'
         ],
-        MaxNumberOfMessages=1,
-        MessageAttributeNames=[
+        "MaxNumberOfMessages": 1,
+        "MessageAttributeNames": [
             'All'
         ],
-        VisibilityTimeout=0,
-        WaitTimeSeconds=10
-    )
+        "VisibilityTimeout": 0,
+        "WaitTimeSeconds": 10
+    }
 
-    if 'Messages' in response:
-        for message in response['Messages']:
-            body = json.loads(message['Body'])
+    def __init__(self, queue_url, *, handlers=None):
+        self.queue_url = queue_url
+        self.handlers = handlers or []
 
-            for record in body['Records']:
-                bucket = record["s3"]["bucket"]["name"]
-                key = record["s3"]["object"]["key"]
+    def add_handler(self, f):
+        self.handlers.append(f)
 
-                metadata = s3.head_object(Bucket=bucket, Key=key)
-                log = f"object=s3://{bucket}/{key} callback={metadata['Metadata']['callback-url']}"
-                print(log)
+    def start_poller(self, *, poll_interval=1, sqs_options=None):
+        options = dict(self.default_sqs_options)  # makes a copy
+        options.update(sqs_options or {})
 
-            sqs.delete_message(
-                QueueUrl=SQS_OUTPUT_QUEUE,
-                ReceiptHandle=message['ReceiptHandle']
-            )
+        while True:
+            response = sqs.receive_message(QueueUrl=self.queue_url, **options)
+            if 'Messages' in response:
+                for message in response['Messages']:
+                    for handler in self.handlers:
+                        handler(message)
 
-    time.sleep(POLL_INTERVAL_SECONDS)
+                sqs.delete_message(
+                    QueueUrl=self.queue_url,
+                    ReceiptHandle=message['ReceiptHandle'])
+
+            time.sleep(poll_interval)
+
+
+def stdout_logger(message):
+    body = json.loads(message['Body'])
+
+    for record in body['Records']:
+        bucket = record["s3"]["bucket"]["name"]
+        key = record["s3"]["object"]["key"]
+
+        metadata = s3.head_object(Bucket=bucket, Key=key)
+        log = f"object=s3://{bucket}/{key} callback={metadata['Metadata']['callback-url']}"
+        print(log)
+
+
+queue = Queue(SQS_OUTPUT_QUEUE)
+queue.add_handler(stdout_logger)
+queue.start_poller(poll_interval=POLL_INTERVAL_SECONDS)
