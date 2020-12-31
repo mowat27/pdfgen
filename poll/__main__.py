8cbed2508224915af95130a9effbca56d0943cc4
Adrian Mowat <adrian.mowat@gmail.com>
2020-12-31 00:26:42 +0000

    Get metadata from generated objects

--- poll/__main__.py
+++ poll/__main__.py
@@ -5,6 +5,7 @@ import time
 from . import SQS_OUTPUT_QUEUE, POLL_INTERVAL_SECONDS
 
 sqs = boto3.client('sqs')
+s3 = boto3.client('s3')
 
 while True:
     response = sqs.receive_message(
@@ -17,7 +18,7 @@ while True:
             'All'
         ],
         VisibilityTimeout=0,
-        WaitTimeSeconds=POLL_INTERVAL_SECONDS
+        WaitTimeSeconds=10
     )
 
     if 'Messages' in response:
@@ -27,7 +28,10 @@ while True:
             for record in body['Records']:
                 bucket = record["s3"]["bucket"]["name"]
                 key = record["s3"]["object"]["key"]
-                print(f"s3://{bucket}/{key} was created")
+
+                metadata = s3.head_object(Bucket=bucket, Key=key)
+                log = f"object=s3://{bucket}/{key} callback={metadata['Metadata']['callback-url']}"
+                print(log)
 
             sqs.delete_message(
                 QueueUrl=SQS_OUTPUT_QUEUE,
