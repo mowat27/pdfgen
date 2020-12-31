dcc666bd0a45c6e4130cd37bd23d8ab94961c479
Adrian Mowat <adrian.mowat@gmail.com>
2020-12-31 20:38:29 +0000

    Extracted methods to make pdf

--- generate/__main__.py
+++ generate/__main__.py
@@ -13,20 +13,29 @@ if not S3_BUCKET_FOR_OUTPUT:
 
 source = sys.argv[1]
 
-if source.startswith("http"):
-    sys.exit("Generating pdfs from URLs is not currently supported")
-else:
-    options = {'enable-local-file-access': None}
-    pdf = pdfkit.from_file(source, False, options=options)
 
+def make_pdf(source):
+    options = {'enable-local-file-access': None}
+    return pdfkit.from_file(source, False, options=options)
+
+
+def upload_to_s3(pdf, bucket):
     s3 = boto3.resource('s3')
     filename = f'generated-{uuid.uuid4()}.pdf'
-    obj = s3.Object(S3_BUCKET_FOR_OUTPUT, filename)
+    obj = s3.Object(bucket, filename)
+
     obj.put(Body=pdf, Metadata={
         "metadata-version": "0.1",
         "generated-by": "pdfgen",
         "callback-url": "https://example.com/docid"
     })
-    print(f'Created s3://{S3_BUCKET_FOR_OUTPUT}/{filename}')
-    print("aws s3api head-object --bucket",
-          S3_BUCKET_FOR_OUTPUT,  "--key", filename)
+
+    print(f'Created s3://{bucket}/{filename}')
+    print("aws s3api head-object --bucket", bucket,  "--key", filename)
+
+
+if source.startswith("http"):
+    sys.exit("Generating pdfs from URLs is not currently supported")
+else:
+    pdf = make_pdf(source)
+    upload_to_s3(pdf, S3_BUCKET_FOR_OUTPUT)
