074ae31b8568081cc2a14784db70be035198eeea
Adrian Mowat <adrian.mowat@gmail.com>
2021-01-01 18:09:25 +0000

    Render content into j2 template

--- generator/daemon/__main__.py
+++ generator/daemon/__main__.py
@@ -2,10 +2,11 @@ import json
 import time
 from signal import SIGINT, signal
 
+import jinja2
 from aws import s3, sqs
 
-from . import POLL_INTERVAL_SECONDS, SQS_INPUT_QUEUE
 from .. import pdf
+from . import POLL_INTERVAL_SECONDS, SQS_INPUT_QUEUE
 
 
 def shutdown(signal, frame):
@@ -18,12 +19,19 @@ signal(SIGINT, shutdown)
 
 def maker(message):
     content = json.loads(message.body['document']['content'])
+    with open('./templates/simple.html.j2', 'r') as f:
+        template = jinja2.Template(f.read())
 
-    print(f"Making ({type(content)}) {content}")
+    html = template.render(content=content)
+
+    with open('templates/index.html', 'w') as f:
+        f.write(html)
 
 
 def logger(message):
-    print(message, flush=True)
+    content = json.loads(message.body['document']['content'])
+    name = content['name']
+    print(f'Making a pdf for {name}', flush=True)
 
 
 sqs.poller.start(queue=SQS_INPUT_QUEUE,
